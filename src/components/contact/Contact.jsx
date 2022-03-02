2196f263a741a19dacfcec0b57090e481a89ac88
JOEL ZAMORA CHAVES <joel.zamora.chaves@est.una.ac.cr>
2022-03-01 19:17:39 -0600

    correcion mapa y parrafos

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faXmark} from '@fortawesome/free-solid-svg-icons';
-import "./contact.scss"
+import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import "./contact.scss";
 
 export default function Contact(){
   return (
     <div className='contact' id='contact'>
-      <div className='wrapper-Map'>
-      <iframe  className='map' frameBorder="" scrolling="no" marginHeight="0" marginWidth="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3929.8507097773204!2d-84.05492821349152!3d9.9463767900414!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcb2e02ac228ce4ec!2sRASE%20BARBER!5e0!3m2!1ses!2sus!4v1645390910457!5m2!1ses!2sus"  title="Rase Barber, Guadalupe" aria-label="Rase Barber, Guadalupe"></iframe>
+      <div className='text'>
+        <p>Estamos situados en Guadalupe, diagonal al AyA, 200m este de la Municipalidad de Goicoechea</p>
       </div>
-  <div className="social-menu">
-  <ul>
-    <li><a href="hola"><FontAwesomeIcon icon={faXmark} size='xl'/></a></li>
-    <li><a href="f"><FontAwesomeIcon icon="fa-brands fa-instagram" /></a></li>
-  </ul>
-</div>
+      <div className='wrapper-Map'>
+        <iframe className='map' frameBorder="" scrolling="no" marginHeight="0" marginWidth="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3929.8507097773204!2d-84.05492821349152!3d9.9463767900414!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcb2e02ac228ce4ec!2sRASE%20BARBER!5e0!3m2!1ses!2sus!4v1645390910457!5m2!1ses!2sus"  title="Rase Barber, Guadalupe" aria-label="Rase Barber, Guadalupe"></iframe>
+      </div>
+      <div className="social-menu">
+        <p>Visitanos en Instagram y contactanos por WhatsApp</p>
+        <ul>
+          <li>
+            <a href="#WhatsApp" target="_blank" rel='noreferrer'>
+              <FontAwesomeIcon icon={faWhatsapp} className='icon' size='3x'/>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/rasebarber/?hl=es-la" target="_blank" rel='noreferrer'>
+              <FontAwesomeIcon icon={faInstagram} className='icon' size='3x' />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import "./intro.scss";
-import background_intro from "../../Images/intro.jpeg";
+
 export default function intro() {
   return (
     <div className="intro" id="intro">
-      <div className="bg-image">
-        <img src={background_intro} alt="" className="img" />
-
-        <div className="mask">
-          <div className="text">
-            <p>
-            En Rase barber shop rescatamos el arte de barberia clasica moderna
-            </p>
-          </div>
-        </div>
+      <div className="text">
+        <p>¡En Rase Barber Shop rescatamos el arte de barberia clasica moderna!</p>
       </div>
     </div>
   );
