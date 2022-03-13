629febdd9271eafc86636a5cace2ca16aa6e9657
DavidTK1198 <ddanielb09@gmail.com>
2022-03-13 15:25:14 -0600

    ajustes

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./intro.scss";
+import { PopupWidget } from "react-calendly";
+
 export default function intro() {
   return (
     <div className="intro" id="Intro">
-      <div className="text">
-        <h2>¡En Rase Barber Shop rescatamos el arte de barberia clasica moderna!</h2>
+      <div className="wrapper">
+        <div className="text">¡En Rase Barber Shop rescatamos el arte de barberia clasica moderna!
+        </div>
       </div>
     </div>
   );
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,18 +1,23 @@
 import "./topbar.scss";
-
+import logo from '../../Images/Rase.png';
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">
-  
-          </a>
+        <a href='#intro'><img src={logo} alt='logo'/></a>
           <div className="itemContainer">
-
+          <a href='#Intro'>Inicio</a>
           </div>
           <div className="itemContainer">
+          <a href='#About'>Quiénes somos</a>
+          </div>
+          <div className="itemContainer">
+          <a href='#Products'>Nuestros productos</a>
+          </div>
+          <div className="itemContainer">
+          <a href='#Contacto'>Contactanos</a>
           </div>
         </div>
         <div className="right">
