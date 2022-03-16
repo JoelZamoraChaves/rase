e9c61a1ddffe56722c1e8a129df293e3796b7ac8
DavidTK1198 <ddanielb09@gmail.com>
2022-03-15 22:01:59 -0600

    cambios esteticos

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -18,14 +18,16 @@ export default function intro() {
             hideGdprBanner: true,
             hideLandingPageDetails: false,
             primaryColor: '00a2ff',
-            textColor: '4d5055'
+            textColor: '4d5055',
+            borderRadius:""
           }}
           rootElement={document.getElementById("root")}
           styles={{
               background:"black",
               color:"white",
-              top: "60%",
-              borderRadius: "12px"
+              top: "75%",
+              borderRadius: "12px",
+              cursor:"pointer"
           }}
           text="Agenda una cita"
           url="https://calendly.com/daniel-barrientos-araya/revision-del-proyecto-1-programacion-iii"
