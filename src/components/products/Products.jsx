a570ff35e8225808ab6c9fb39220af8fb517e3c8
DavidTK1198 <ddanielb09@gmail.com>
2022-03-27 13:08:01 -0600

    ajuste productos

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,28 +2,37 @@ import React from "react";
 import minoxidil from "../../Images/Minoxidil.jpeg";
 import ceras from "../../Images/Ceras.jpeg";
 import "./products.scss";
+
 export default function Products() {
   return (
     <div className="products" id="Products">
-        <div className="title"><b>Productos disponibles en Rase Barber Shop</b></div>
-        <div className="card">
-            <div className="container">
-                <img src={minoxidil} alt="Minoxidil"/>
-            </div>
-            <div className="details">
-                <h3>Minoxidil</h3>
-                <p></p>
-            </div>
+     <div className="title">
+        <b>Productos disponibles en Rase Barber Shop</b>
+      </div>
+     <div className="wrapper">
+     <div className="card-center">
+      <div className="card">
+        <div className="container">
+          <img src={minoxidil} alt="Minoxidil" />
         </div>
-        <div className="card">
-            <div className="container">
-                <img src={ceras} alt="Ceras"/>
-            </div>
-            <div className="details">
-                <h3>Ceras</h3>
-                <p></p>
-            </div>
+        <div className="details">
+          <h3>Minoxidil</h3>
+          <p></p>
         </div>
+      </div>
+      </div>
+      <div className="card-center">
+      <div className="card">
+        <div className="container">
+          <img src={ceras} alt="Ceras" />
+        </div>
+        <div className="details">
+          <h3>Ceras</h3>
+          <p></p>
+        </div>
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
